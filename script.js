c9e3dee73caa706f882859082bf99f97ff310cbf
Molten-Sulfur <97858539+Molten-Sulfur@users.noreply.github.com>
2022-02-18 16:14:51 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+require([
+      "esri/Map",
+      "esri/layers/FeatureLayer",
+      "esri/views/MapView",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      FeatureLayer,
+      MapView
+    ) {
+
+      // Create the map
+      var map = new Map({
+        basemap: "gray"
+      });
+
+      // Create the MapView
+      var view = new MapView({
+        container: "stlDiv",
+        map: map,
+        center:[-90.26, 38.64],
+        zoom: 11
+      });
+
+      /*************************************************************
+       * The PopupTemplate content is the text that appears inside the
+       * popup. {fieldName} can be used to reference the value of an
+       * attribute of the selected feature. HTML elements can be used
+       * to provide structure and styles within the content. The
+       * fieldInfos property is an array of objects (each object representing
+       * a field) that is use to format number fields and customize field
+       * aliases in the popup and legend.
+       **************************************************************/
+
+      var template = { // autocasts as new PopupTemplate()
+        title: "Neighborhood: {NHD_NAME}",
+        content: [{
+          // It is also possible to set the fieldInfos outside of the content
+          // directly in the popupTemplate. If no fieldInfos is specifically set
+          // in the content, it defaults to whatever may be set within the popupTemplate.
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "NHD_NUM",
+            label: "Neighborhood: ",
+            visible: true
+          }]
+        }]
+      };
+
+ 
+  var renderer = {
+    };
+  
+  
+      // Reference the popupTemplate instance in the
+      // popupTemplate property of FeatureLayer
+      var featureLayer = new FeatureLayer({
+        url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/ArcGIS/rest/services/STL_Neighborhood/FeatureServer/0",
+        outFields: ["*"],
+        popupTemplate: template,
+        renderer:renderer
+      });
+  
+      map.add(featureLayer);
+  
+
+      featureLayer.renderer = {
+      type: "simple",  // autocasts as new SimpleRenderer()
+      symbol: {
+        type: "simple-fill",  // autocasts as new SimpleMarkerSymbol()
+        size: 6,
+        color: "transparent",
+        outline: {  // autocasts as new SimpleLineSymbol()
+          width: 2,
+          color: "red"
+        }
+      }
+    };
+    });
